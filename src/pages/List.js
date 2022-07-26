01fbe98edefb959d20447ee5b74fd064ea865350
KendSoto02 <sotofajardo05@gmail.com>
2022-07-26 12:05:45 -0600

    APP DONE

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './List.css';
 
 const List = (props) => {
     const {contents} = props;
@@ -30,19 +31,47 @@ const List = (props) => {
             return(
                 <ul>
                     {contents.slice(0,10).map((content) => {
-                        if(content.statusOrder != "delayed"  && content.statusOrder != "delivered"){ 
-                            return (
-                                <div className="div-list">
-                                    <li key={content.id}>
-                                        <span className="Component-Title"> <b>ID Pedido: </b> {content.id} 
-                                        <b> | Cliente: </b> {content.nameClient} 
-                                        <b> | Plato: </b> {content.nameDish}
-                                        <b> | Cantidad: </b> {content.quantity}
-                                        <b> | Fecha: </b> {content.date}
-                                        <b> | Estatus: </b> {content.statusOrder}</span>
-                                    </li>
-                                </div>
-                            )
+                        if(content.statusOrder != "anuled"  && content.statusOrder != "delivered"){
+                            if(content.statusOrder === "onTime"){ 
+                                return (
+                                    <div className="onTime">
+                                        <li key={content.id}>
+                                            <span className="Component-Title"> <b>ID Pedido: </b> {content.id} 
+                                            <b> | Cliente: </b> {content.nameClient} 
+                                            <b> | Plato: </b> {content.nameDish}
+                                            <b> | Cantidad: </b> {content.quantity}
+                                            <b> | Fecha: </b> {content.date}
+                                            <b> | Estatus: </b> {content.statusOrder}</span>
+                                        </li>
+                                    </div>
+                                )
+                            }else if(content.statusOrder === "overTime"){ 
+                                return (
+                                    <div className="overTime">
+                                        <li key={content.id}>
+                                            <span className="Component-Title"> <b>ID Pedido: </b> {content.id} 
+                                            <b> | Cliente: </b> {content.nameClient} 
+                                            <b> | Plato: </b> {content.nameDish}
+                                            <b> | Cantidad: </b> {content.quantity}
+                                            <b> | Fecha: </b> {content.date}
+                                            <b> | Estatus: </b> {content.statusOrder}</span>
+                                        </li>
+                                    </div>
+                                )
+                            }else if(content.statusOrder === "delayed"){ 
+                                return (
+                                    <div className="delayed">
+                                        <li key={content.id}>
+                                            <span className="Component-Title"> <b>ID Pedido: </b> {content.id} 
+                                            <b> | Cliente: </b> {content.nameClient} 
+                                            <b> | Plato: </b> {content.nameDish}
+                                            <b> | Cantidad: </b> {content.quantity}
+                                            <b> | Fecha: </b> {content.date}
+                                            <b> | Estatus: </b> {content.statusOrder}</span>
+                                        </li>
+                                    </div>
+                                )
+                            }
                         }
                         
                     })}
